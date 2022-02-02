05f2655a3ec161747af41d59cd6cfe16b8a7bab4
junghoonkoo <jh7109@hotmail.com>
2022-02-02 15:57:09 +0900

    connecting react

---
+++ src/main/java/com/factos/web/MainController.java
@@ -0,0 +1,24 @@
+package com.factos.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/factos/Main")
+    public String Test(){
+        return "현재 서버시간은 "+new Date() +"입니다. \n";
+    }
+
+    public class HelloController {
+        @GetMapping("/factos/testReact")
+        public List<String> testReact(){
+            return Arrays.asList("서버 포트는 8080", "리액트 포트는 3000");
+        }
+    }
+}
