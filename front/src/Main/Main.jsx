67f891ceed2b8b526b4a47c75ef3f1860104ac6a
junghoonkoo <jh7109@hotmail.com>
2022-02-05 02:34:46 +0900

    mainCommit

--- front/src/Main/Main.jsx
+++ front/src/Main/Main.jsx
@@ -1,1 +1,16 @@
-//ddddd
+import React, { Component} from 'react'
+import Layout from "../Common/Layout";
+
+
+class Main extends Component{
+    render() {
+        return (
+            <div>
+                <Layout></Layout>
+            </div>
+
+        );
+    }
+}
+
+export default Main
