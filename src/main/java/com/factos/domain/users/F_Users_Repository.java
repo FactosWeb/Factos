73e28d4105eb1fd42b86403401b52f0b0452bac6
jaeseok <jssjchl100@gmail.com>
2022-06-28 00:25:36 +0900

    jpa 확인 필요

--- src/main/java/com/factos/domain/users/F_Users_Repository.java
+++ src/main/java/com/factos/domain/users/F_Users_Repository.java
@@ -10,7 +10,7 @@ public interface F_Users_Repository extends JpaRepository<fUser, String> {
     Optional<fUser> findByUserId(fUser paramData); //오류남 jpa 확인필요
 
 }
-//DB Layer 접근   자
+//DB Layer 접근자
 //인터페이스로 생성
 //JPARepository <Entity 클래스, PK타입> 상속 == 기본적인 CRUD 구동
 //Entity 클래스와 같은 위치에 존재해야한다.
